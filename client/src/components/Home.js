d3e5efb9c48fa48cb38936ca6c0f6afc218dfb1f
Zayd <zayd.sarraj@gmail.com>
2020-08-14 22:02:50 -0400

    fixed subtitle formatting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,19 +12,19 @@ const Home = (props) => {
             <Container className='text-center mx-auto py-2 border rounded border-dark shadow mt-3 whiteshade'>
                 <h1>Zayd Sarraj - Full Stack Developer</h1>
                 <Row className="mx-5">
-                    <Col sm="3" className='text-center mx-auto py-2 border rounded border-dark shadow mt-3 whiteshade'>
+                    <Col sm="2" className='text-center mx-auto py-2 border rounded border-dark shadow mt-3 whiteshade'>
                         <div className="textshadows"><a href="https://github.com/Zayd-Sarraj" class="fa fa-github text-dark"></a></div>
                     </Col>
-                    <Col sm="3" className='text-center mx-1 py-2 border rounded border-dark shadow mt-3 whiteshade'>
+                    <Col sm="3" className='text-center mx-ato py-2 border rounded border-dark shadow mt-3 whiteshade'>
                         <div className="emaildiv">
                             <span className="email textshadows">@</span>
                         </div>
                     </Col>
-                    <Col sm="3" className='text-center mr-1 py-2 border rounded border-dark shadow mt-3 whiteshade'>
-                        <div className="textshadows"> <a href="https://www.linkedin.com/in/zayd-sarraj-445930196/" class=" text-dark mx-2 fa fa-linkedin"></a></div>
-                    </Col>
                     <Col sm="3" className='text-center mx-auto py-2 border rounded border-dark shadow mt-3 whiteshade'>
                         <div className="textshadows"><a href="http://zayd-sarraj.herokuapp.com/resume/ZS_Resume_2020.pdf" class="text-dark"><b>Resume</b></a></div>
+                    </Col>
+                    <Col sm="2" className='text-center mx-auto py-2 border rounded border-dark shadow mt-3 whiteshade'>
+                        <div className="textshadows"> <a href="https://www.linkedin.com/in/zayd-sarraj-445930196/" class=" text-dark mx-2 fa fa-linkedin"></a></div>
                     </Col>
                 </Row>
             </Container>
